555d5d1a87a9bfe6565ce935b1ecd41a3f05713c
Mikael Rinnetmäki <mikael@sensotrend.com>
2023-05-17 14:02:15 +0300

    Add description for Wellmo

--- fhir-demo-2023/src/pages/wellmo.js
+++ fhir-demo-2023/src/pages/wellmo.js
@@ -4,6 +4,7 @@ import Demo from '../components/Demo';
 import Features from '../components/Features';
 import { demos } from '../config/data';
 import wellmoLogo from '../images/wellmo.png';
+import wellmoSolution from '../images/wellmo.webp';
 
 export const Head = () => <title>FHIR Demo 2023: Wellmo</title>;
 
@@ -18,6 +19,23 @@ export default function Wellmo({ location }) {
         </a>
       </h1>
       <Features list={features} />
+      <section id="about">
+        <h2>Platform for digital health services</h2>
+        <p>Wellmo is an ultimate platform for personalised digital health services. Wellmo provides
+        all the elements of an ecosystem platform, including integrations to hundreds of connected
+        health devices, white label mobile app, a content management system and integration
+        APIs.</p>
+        <p>Effectively engage your customers in their own health, become more relevant, attract new
+        welcustomers and save health care costs.</p>
+        <img src={wellmoSolution} alt="" />
+      </section>
+      <section id="smart-in-two-ways">
+        <h2>SMART App Launch in Two Ways</h2>
+        <p>Wellmo can be integrated into electronic health record systems through the <strong>SMART
+        App Launch</strong> mechanism.</p>
+        <p>Wellmo can also act as a cover app integrating many other health and wellbeing apps. In
+        this case, the apps can be launched from Wellmo using SMART.</p>
+      </section>
       <section id="contact">
         <h3>Contact</h3>
         <div className="business-card">
