69b5acaf05394830ad15e102e835c7c200500ee9
Mikael Rinnetmäki <mikael@sensotrend.com>
2023-05-19 10:21:11 +0300

    Add demo description for Apotti

--- fhir-demo-2023/src/pages/apotti.js
+++ fhir-demo-2023/src/pages/apotti.js
@@ -43,7 +43,65 @@ export default function Apotti({ location }) {
           </ul>
         </strong>
       </aside>
-      <p>Demo description will be added soon.</p>
+      <section id="kyvykkyydet">
+        <h2>Apotti-järjestelmän kyvykkyydet ja kehitys</h2>
+        <p>Apotti-järjestelmä on kattavin ja edistyksellisin ratkaisu sosiaali- ja terveydenhuollon
+        tarpeisiin. Meillä on jo nyt käytössämme erikoissairaanhoidon, perusterveydenhuollon ja
+        sosiaalihuollon tiedot yhdistävä, kyvykäs ja jatkuvasti kehittyvä asiakas- ja potilastieto-
+        sekä toiminnanohjausjärjestelmä. Apotti-järjestelmää ja sen käytettävyyttä kehitetään
+        jatkuvasti tiiviissä yhteistyössä Apotin omistajaorganisaatioiden kanssa, mm. Apotti
+        2.0-kehitysohjelmassa.</p>
+        <p>Apotin yhteistyö Epicin kanssa tuo edistyneimmät kansainväliset ratkaisut Apotin
+        asiakasorganisaatioiden saataville. Epic tekee yhteistyötä alan merkittävimpien
+        toimijoiden, kuten Microsoftin, OpenAI:n ja Applen kanssa.</p>
+      </section>
+      <section id="ekosysteemi">
+        <h2>Apotin ekosysteemitoiminta ja verkostokehittäminen</h2>
+        <p>Apotti-järjestelmän ytimenä toimiva Epic on käytössä maailman parhaissa sairaaloissa eri
+        puolilla maailmaa. Apotin ja Epicin yhteistyö tarjoaa suomalaisille sosiaali- ja
+        terveysteknologian yrityksille väylän kehittää palveluita ja sovelluksia niin kotimaisille
+        kuin kansainvälisille markkinoille. Olemme avanneet Apotti-ekosysteemin kehitys- ja
+        testausympäristön, jossa yritykset pääsevät turvallisesti testaamaan omia palveluitaan
+        kuvitteellisella asiakas- ja potilasdatalla.</p>
+      </section>
+      <section id="demo">
+        <h2>Apotin ekosysteemin demo ATK-päivillä</h2>
+        <p>Demossa kuvataan Apotti-järjestelmän ja sovellusten yhteistoiminta <strong>SMART App
+        Launch</strong> -käynnistyksenä (vanhalta nimeltään SMART on FHIR), jolloin Apottiin
+        kirjautunut käyttäjä näkee yhdellä kirjautumisella myös sovelluksen tuottaman tiedon.</p>
+        <p>Demossa näytetään:</p>
+        <ul>
+          <li>
+            <Link to="../sensotrend">Sensotrend</Link> (Apotin ekosysteemikumppani): rooli =
+            diabetespotilas
+            <ul>
+              <li>
+                näkymä: potilaan näkymät tabletin kautta (Sensotrendin näkymä on kansainvälisesti
+                harmonisoitu ambulatoorinen glukoosiprofiili, jota diabetesta hoitavat
+                terveydenhuollon ammattilaiset osaavat tulkita.)
+              </li>
+            </ul>
+          </li>
+          <li>
+            Apotti: rooli = Apotti-järjestelmää käyttävä sisätautilääkäri
+            <ul>
+              <li>
+                näkymä: sisätautilääkärin työnkulkua diabetespotilaan hoidossa, missä on mukana
+                Sensotrendin näkymä saumattomasti upotettuna
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </section>
+      <section id="background">
+        <h2>Taustaa</h2>
+        <p>Apotti on modulaarinen järjestelmä, johon on aiempaa helpompaa liittää
+        terveys&shy;teknologia&shy;yritysten/startupien kehittämiä sovelluksia <strong>SMART App
+        Launch</strong> -teknologialla. Näin Apotti-järjestelmään kirjautunut ammattilaiskäyttäjä
+        näkee yhdellä kirjautumisella myös sovelluksen tuottaman tiedon. Tämä tehostaa toimintaa ja
+        vähentää virheiden todennäköisyyttä, koska ammattilaiskäyttäjän ei tarvitse manuaalisesti
+        poimia tarvitsemiaan tietoja eri palveluista tai sovelluksista.</p>
+      </section>
       <section id="contact">
         <h3>Contact</h3>
         <div className="business-card">
