38d71566045e95e6c327118db82c94d7725ed646
Mikael Rinnetmäki <mikael@sensotrend.com>
2023-05-19 10:28:47 +0300

    Add demo description for Una
    This will be updated and translated into English later today...

--- fhir-demo-2023/src/pages/una.js
+++ fhir-demo-2023/src/pages/una.js
@@ -20,8 +20,33 @@ export default function Una({ location }) {
       <Features list={features} />
       <aside className="event atk-paivat">
         <h2>ATK-päivät: Ständi 210</h2>
+        <p>UNA on mukana sosiaali- ja terveydenhuollon ATK-päivillä järjestettävässä FHIR Demossa.
+        Tervetuloa ständillemme C.210 keskustelemaan FHIRistä ja muista mahdollisuuksistamme auttaa
+        saamaan sote-data hyödynnettäväksi.</p>
       </aside>
-      <p>Demo description will be added soon.</p>
+      <section id="demo">
+        <h2>Demo</h2>
+        <p>UNA on toteuttanut FHIR Demoon 2023 konkreettisen ja testattavissa olevan kansallisia
+        FHIR perusprofiileja hyödyntävän tapahtumien FHIR-rajapinnan, jossa yhdistellään Ydin
+        UNA:ssa tietoja useista eri tietolähteistä. Rajapinnan tuottama tieto on lisäksi tuotu
+        hyödynnettäväksi <Link to="../bcb-medical">BCB Medicalin</Link> toteuttamaan
+        Omavointi-mobiilisovellukseen.</p>
+        <div className="media">
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/CRt6nMQL6O4"
+            title="UNA FHIR Demo 2023"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen>
+          </iframe>
+        </div>
+      </section>
+      <section id="smart">
+        <h2>SMART App Launch</h2>
+        <p>UNAssa ollaan tekemässä myös sovellusten käynnistys SMART App Launch -toteutuksena.</p>
+      </section>
       <section id="contact">
         <h3>Contact</h3>
         <div className="business-card">
