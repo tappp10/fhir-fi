19c01d0db13f69ce3625790426edeb8d6ddd32d0
Mikael Rinnetmäki <mikael@sensotrend.com>
2023-05-20 12:39:53 +0300

    Delete extra 'also'

--- fhir-demo-2023/src/pages/index.js
+++ fhir-demo-2023/src/pages/index.js
@@ -106,9 +106,8 @@ export default function IndexPage({ location }) {
           <p>
             We're also planning a tour at least to the Finnish Wellbeing Services Counties
             (<i>hyvinvointialue</i>, known as Regions in other Nordic countries) in the autumn.
-            We're also happy to extend this demo to other audiences. If you're interested in having
-            the FHIR Demo presented to you or your people, please <a href="#contact">get in
-            touch!</a>
+            We're happy to extend this demo to other audiences. If you're interested in having the
+            FHIR Demo presented to you or your people, please <a href="#contact">get in touch!</a>
           </p>
         </section>
         <section>
