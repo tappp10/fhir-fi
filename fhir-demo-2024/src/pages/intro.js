41716f0e718a188bcaa6ec81b1d65044cf53725a
Mikael Rinnetmäki <mikael@sensotrend.com>
2024-03-27 09:44:54 +0200

    Attempt to make the image url dynamic
    Does not work this way either. Perhaps with a useEffect hook?

--- fhir-demo-2024/src/pages/intro.js
+++ fhir-demo-2024/src/pages/intro.js
@@ -3,21 +3,22 @@ import { Link, withPrefix } from 'gatsby';
 
 import Article, { getHead } from '../components/Article';
 
-const Intro = () => {
+
+export function Head() {
+  const title = <title>FHIR Demo 2024 by HL7 Finland - WELCOME!</title>;
+  return getHead({ title });
+};
+
+export default function Intro() {
+  const imageFolder = withPrefix('/images');
+  const logoSrc = `${imageFolder}/hl7-finland-animated.svg?d=${Date.now()}`;
   return (
     <Article id="intro">
       <Link to="/">
-        <img src={`${withPrefix('/images/hl7-finland-animated.svg')}?d=${Date.now()}`} alt="HL7 Finland" />
+        <img src={logoSrc} alt="HL7 Finland" />
         <p>presents</p>
         <h1>FHIR Demo 2024</h1>
       </Link>
     </Article>
   )
-};
-
-export default Intro;
-
-export function Head() {
-  const title = <title>FHIR Demo 2024 by HL7 Finland - WELCOME!</title>;
-  return getHead({ title });
 };
