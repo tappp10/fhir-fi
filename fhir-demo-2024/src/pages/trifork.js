cf3f70a2edd16ebc421f014677c651582d159552
Jens Kristian Villadsen <jenskristianvilladsen@gmail.com>
2024-04-22 19:54:19 +0200

    Added some text

--- fhir-demo-2024/src/pages/trifork.js
+++ fhir-demo-2024/src/pages/trifork.js
@@ -24,7 +24,7 @@ export default function Trifork({ location }) {
       </h1>
       <Roles list={roles} />
       <section id="intro">
-        <p>Trifork offers both Platform-as-a-Service (PaaS) and consultancy.</p>
+        <p>Trifork offers Platform-as-a-Service (PaaS) and consultancy.</p>
         <h2>Platform</h2>
         <ul>
           <li><Link to="#thp">Trifork Health Platform</Link></li>
@@ -41,28 +41,39 @@ export default function Trifork({ location }) {
       <section id="thp">
         <h2>Trifork Health Platform</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at diam vehicula, semper ligula ac, auctor justo. Nunc in felis aliquet, viverra erat aliquet, accumsan sapien. Quisque elementum congue purus at mattis. Etiam volutpat porta urna, non faucibus nibh lacinia nec. Phasellus sed nisl blandit leo ultrices sagittis at quis mauris. Phasellus eget dolor nec justo aliquet ultrices non nec lorem. Etiam tortor eros, tempor ut sollicitudin a, dignissim in diam.
+          The Trifork Health Platform is a curated piece of FHIR infrastructure based on existing open source components. It brings the best of the open source world together with the knowhow and expertise of Trifork's competences and love for agnostic cloud technology. The Trifork Health Platform focuses on the following key points:
         </p>
+        <ul>
+          <li>
+            <strong>Scalablility:</strong> Central pieces of infrastructure needs scaling and so does the Trifork Health Platform. That's why we've combined it with the use of Apache Kafka and the agnosticism of Kubernetes.
+          </li>
+          <li>
+            <strong>Security:</strong> Having deployed, maintained and run several different projects with or without FHIR the experience of running large scale infrastructure comes down to the same set of compentences. Trifork brings that expertise to its Health Platform and reuses all the knowhow and knowledge obtained from having done this since the dawn of the Danish Shared Medication Record in 2013.
+          </li>
+          <li>
+            <strong>Openness:</strong> The Trifork Health Platform is as open about its API's as the FHIR specifiction aims to be.
+          </li>
+        </ul>
       </section>
 
       <section id="fut">
         <h2>FUT</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at diam vehicula, semper ligula ac, auctor justo. Nunc in felis aliquet, viverra erat aliquet, accumsan sapien. Quisque elementum congue purus at mattis. Etiam volutpat porta urna, non faucibus nibh lacinia nec. Phasellus sed nisl blandit leo ultrices sagittis at quis mauris. Phasellus eget dolor nec justo aliquet ultrices non nec lorem. Etiam tortor eros, tempor ut sollicitudin a, dignissim in diam.
+          Trifork is one of vendors having built the future telemedicine infrastructure in Denmark called FUT. The infrastructure is heavily based on FHIR and aims to support current and future telemedicine applications on top of the infrastructure. The documentation to the infrastructure can be found <a href="https://docs.ehealth.sundhed.dk/latest/ig/index.html">here</a>. The FUT infrastructure uses roughly +40 different FHIR resource profiles and integrates to +15 danish national registries and systems. The FUT infrastructure is owned and governed by Danish municipalities and Danish regions.
         </p>
       </section>
 
       <section id="gravitate">
         <h2>Gravitate Health</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at diam vehicula, semper ligula ac, auctor justo. Nunc in felis aliquet, viverra erat aliquet, accumsan sapien. Quisque elementum congue purus at mattis. Etiam volutpat porta urna, non faucibus nibh lacinia nec. Phasellus sed nisl blandit leo ultrices sagittis at quis mauris. Phasellus eget dolor nec justo aliquet ultrices non nec lorem. Etiam tortor eros, tempor ut sollicitudin a, dignissim in diam.
+        The Gravitate Health is a public – private partnership with 41 members from Europe and the US, co-led by University of Oslo (coordinator) and Pfizer (industry lead), funded by the Innovative Medicines Initiative (IMI) – a joint undertaking of the European Commission, the European Federation of Pharmaceutical Industries and Associations (EFPIA), IMI2 Associated Partners. Triforks role in Gravitate Health is to provide expert HL7 FHIR competences in building the future international standard for leafleats (found <a href="https://hl7.org/fhir/uv/emedicinal-product-info/">here</a>) as well as extending the danish Medicin Card app with a a prototype supporting the Gravitate Health specific "$lens" operation found <a href="https://build.fhir.org/ig/hl7-eu/gravitate-health/index.html">here</a>  on top of existing danish infrastructure.
         </p>
       </section>
 
       <section id="rkkp">
         <h2>Regionernes Kliniske Kvalitetsudviklingsprogram (RKKP)</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at diam vehicula, semper ligula ac, auctor justo. Nunc in felis aliquet, viverra erat aliquet, accumsan sapien. Quisque elementum congue purus at mattis. Etiam volutpat porta urna, non faucibus nibh lacinia nec. Phasellus sed nisl blandit leo ultrices sagittis at quis mauris. Phasellus eget dolor nec justo aliquet ultrices non nec lorem. Etiam tortor eros, tempor ut sollicitudin a, dignissim in diam.
+        Trifork has successfully transformed RKKP’s quality reporting platform from a traditional data model to the Fast Healthcare Interoperability Resources (FHIR) model, which exemplifies the next era of healthcare data in Denmark and in the modern, data-driven world. We have also successfully inspired them in container-based architecture, and the platform is both futureproof and easier to manage for the customer and is also more more cost-efficient. The platform primarily uses Questionnaires and QuestionnaireResponses and maintains and curates all +300 valuesets and codesystems in designated FHIR Implementation Guides.
         </p>
       </section>
 
