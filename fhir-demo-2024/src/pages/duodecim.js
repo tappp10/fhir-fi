70df561091324e587f0627a1658f27a3a6583603
Mikael Rinnetmäki <mikael@sensotrend.com>
2024-05-11 00:44:34 +0300

    Add content for Duodecim

--- fhir-demo-2024/src/pages/duodecim.js
+++ fhir-demo-2024/src/pages/duodecim.js
@@ -22,8 +22,26 @@ export default function Duodecim({ location }) {
         </a>
       </h1>
       <Roles list={roles} />
-      <section className="work-in-progress">
-        <p>Participation confirmed, showcase content to be added.</p>
+      <section id="about">
+        <p>
+          Duodecim demonstrates the use of CDS Hooks services for providing real-time Clinical
+          Decision Support. The CDS Hooks concept can radically change how Clinical Decision
+          Support is used in the health record systems. It enables a new level of
+          context-sensitivity and support for clinician's workflow.
+        </p>
+        <p>
+          Duodecim has been on the CDS market for over a decade with its EBMEDS Clinical Decision
+          Support solution with integrations to several health record systems. It returns
+          reminders, therapeutic suggestions (including drug databases), and links to guidelines.
+          It also provides various tools, such as clinical calculators. EBMEDS is a medical device,
+          class IIa, according to the EU MDR 2017/745.
+        </p>
+        <p>
+          Our technological strategy relies firmly on HL7 FHIR. We expect to provide an increasing
+          amount of clinical content completely based on FHIR. In <a
+          href="https://fhir.fi/en/demo2023/duodecim/">FHIR demo 2023</a> we implemented SMART App
+          Launch support for calculators and forms.
+        </p>
       </section>
       <section id="contact">
         <h3>Contact</h3>
