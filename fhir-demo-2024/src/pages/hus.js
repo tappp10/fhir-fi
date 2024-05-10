c728d5a6875e8dceffd80a70ae8d9c639a6e0405
Mikael Rinnetmäki <mikael@sensotrend.com>
2024-05-10 14:12:01 +0300

    Add proposed content for HUS

--- fhir-demo-2024/src/pages/hus.js
+++ fhir-demo-2024/src/pages/hus.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 import { getHead } from '../components/Article';
 import Demo from '../components/Demo';
@@ -31,20 +32,30 @@ export default function HUS({ location }) {
         </p>
       </section>
       <section id="fhir">
+        <h2>FHIR First</h2>
         <p>
           FHIR-capable information systems are directly interoperable - and there is no longer need
           for traditional integrations. The end result is a revolutionary improvement in the speed
           and cost-effectiveness of implementations. 
         </p>
         <p>
-          For this reason, HUS has made the "FHIR FIRST" policy, on the basis of which HUS
-          primarily uses and requires from its partners standard interfaces in accordance with the
-          FHIR standard in implementing the interoperability (integrations) of information systems.
+          For this reason, HUS has created the <strong>FHIR FIRST</strong> policy, on the basis of
+          which HUS primarily uses and requires from its partners standard interfaces in accordance
+          with the FHIR standard in implementing the interoperability (integrations) of information
+          systems.
         </p>
         <p>
           By using the FHIR standard, which is based on an international standard, HUS also
           supports the access of Finnish startup companies and other operators to international
           markets.
+        </p>
+      </section>
+      <section id="collaboration">
+        <h2>Collaboration</h2>
+        <p>
+          Success depends on national cooperation in uniform use of the standard. HUS considers it
+          very important that the harmonization work carried out within the framework of <Link
+          to="/hl7-finland">HL7 Finland</Link> progresses rapidly.
         </p>
       </section>
     </Demo>
